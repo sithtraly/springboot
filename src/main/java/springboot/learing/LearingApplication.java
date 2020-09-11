3fe55593d2a5243f10bd5ee852d2565ec3a2c986
sithtraly <70559804+sithtraly@users.noreply.github.com>
2020-09-11 15:08:24 +0700

    Initialize project

---
+++ src/main/java/springboot/learing/LearingApplication.java
@@ -0,0 +1,17 @@
+package springboot.learing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author sith_traly
+ *
+ */
+@SpringBootApplication
+public class LearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingApplication.class, args);
+	}
+
+}
